aa2d7f08351a7ff01ce1515ff8524657c527e264
Rick Donelson <rick.donelson@gmail.com>
2014-06-14 22:26:42 -0400

    Initial commit of problems 1-3.

---
+++ Problem1.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Multiples of 3 and 5 - http://projecteuler.net/problem=1
+ *
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5,
+ * we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+$limit = 1000;
+$sum = 0;
+
+for ($i = 1; $i < $limit; $i++) {
+    if (($i % 3 == 0) || ($i % 5 == 0)) {
+        $sum += $i;
+    }
+}
+
+echo 'Sum: ' . $sum . PHP_EOL;
